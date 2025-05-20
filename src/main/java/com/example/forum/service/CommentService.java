5cf4e5ed718542a39cd6dddbc9dda3b8e46a2eee
旭廉 <asahi.ren@alhinc.jp>
2025-05-20 17:51:41 +0900

    Javaフレームワーク応用課題（匿名掲⽰板の機能追加）1. 投稿へのコメント機能を追加まで進みました。

---
+++ src/main/java/com/example/forum/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.example.forum.service;
+
+import com.example.forum.controller.form.CommentForm;
+import com.example.forum.repository.CommentRepository;
+import com.example.forum.repository.entity.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+    @Autowired
+    CommentRepository commentRepository;
+    /*
+     * レコード追加
+     */
+    public void saveComment(CommentForm reqComment) {
+        Comment saveComment = setCommentEntity(reqComment);
+        commentRepository.save(saveComment);
+    }
+    /*
+     * リクエストから取得した情報をEntityに設定
+     */
+    private Comment setCommentEntity(CommentForm reqComment) {
+        Comment comment = new Comment();
+        comment.setId(reqComment.getId());
+        comment.setContent(reqComment.getContent());
+        return comment;
+    }
+
+}
